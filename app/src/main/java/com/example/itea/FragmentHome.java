95d1148b3d8fd91564f5dc8913a571be0bf497b1
Katrine Christensen <katch@itu.dk>
2022-04-19 14:17:49 +0200

    Add template for subpages

--- app/src/main/java/com/example/itea/FragmentHome.java
+++ app/src/main/java/com/example/itea/FragmentHome.java
@@ -4,18 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class FragmentHome extends Fragment {
     TextView text;
+    Button buttonToHome2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         text = v.findViewById(R.id.fragment_name_text);
+        buttonToHome2 = v.findViewById(R.id.button_to_home2);
+
+        buttonToHome2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_Home_to_Home2);
+            }
+        });
 
         return v;
     }
--- app/src/main/java/com/example/itea/MainActivity.java
+++ app/src/main/java/com/example/itea/MainActivity.java
@@ -15,13 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        // Find the NavController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //val navController = this.findNavController(R.id.nav_host_fragment)
-
         // Find reference to bottom navigation view
         BottomNavigationView navView= findViewById(R.id.bottom_nav_view);
-
-        // Hook your navigation controller to bottom navigation view
+        // Hook navigation controller to bottom navigation view
         NavigationUI.setupWithNavController(navView, navController);
 
     }
