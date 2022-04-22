d3c33982191cffea979d70d9d3c4790c822e248c
Katrine Christensen <katch@itu.dk>
2022-04-22 14:56:30 +0200

    Add shopping fragment

---
+++ app/src/main/java/com/example/itea/FragmentShopping.java
@@ -0,0 +1,22 @@
+package com.example.itea;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentShopping extends Fragment {
+    TextView text;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_shopping, container, false);
+
+        text = v.findViewById(R.id.fragment_name_text);
+
+        return v;
+    }
+}
